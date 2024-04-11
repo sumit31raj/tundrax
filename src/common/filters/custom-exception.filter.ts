0aad45ce14a05b239256ce6d0c6bb2cd8a68e7f0
sumit31raj <sumit31raj@gmail.com>
2024-04-11 11:24:24 +0530

    Update custom exception and logging interceptor

---
+++ src/common/filters/custom-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from "@nestjs/common";
+
+@Catch()
+export class CustomExceptionFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    let statusCode = 500;
+    let errorMessage=exception.message;
+
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus()
+      const exceptionResp: any = exception.getResponse()
+      errorMessage = exceptionResp.message
+    }
+
+    response.status(statusCode).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: errorMessage,
+      statusCode,
+    });
+  }
+}
--- src/core/interceptors/logging.interceptor.ts
+++ src/core/interceptors/logging.interceptor.ts
@@ -3,18 +3,32 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    console.log("Before...");
+
+    const request = context.switchToHttp().getRequest();
+    const { url, method, params, query, body } = request;
+    // Filter out sensitive information like password from the request body
+    const filteredBody = { ...body };
+    if (filteredBody.password) {
+      filteredBody.password = "[FILTERED]";
+    }
+    console.log("Request:", { url, method, params, query, body: filteredBody });
 
     const now = Date.now();
-    return next
-      .handle()
-      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+    return next.handle().pipe(
+      tap((data) => {
+        console.log("Response:", {
+          statusCode: context.switchToHttp().getResponse().statusCode,
+        });
+        console.log(`After... ${Date.now() - now}ms`);
+      }),
+    );
   }
 }
